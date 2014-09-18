ce6174beaa306d614592a181d4bab82a42e1e75a
Jonatan Ruohomäki <jitan.elis@gmail.com>
2014-09-18 15:44:51 +0200

    Fixing gamflow..

--- js/main.js
+++ js/main.js
@@ -7,10 +7,14 @@ var GAME_OVER = false;
 function createDeck(numberOfDecks) {
 	var newDeck = [];
 	for (var x = 0; x < numberOfDecks; x++) {
-		newDeck.push('AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC');
-		newDeck.push('AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH');
-		newDeck.push('AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD');
-		newDeck.push('AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS');
+		newDeck.push('AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC',
+			'JC', 'QC', 'KC');
+		newDeck.push('AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH',
+			'JH', 'QH', 'KH');
+		newDeck.push('AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD',
+			'JD', 'QD', 'KD');
+		newDeck.push('AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS',
+			'JS', 'QS', 'KS');
 	}
 	return newDeck;
 }
@@ -32,6 +36,33 @@ function shuffle(array) {
 	}
 
 	return array;
+}
+
+function getValue(cards) {
+	var value = 0;
+	var numAces = 0;
+
+	// Check through cards and assign value
+	for (var x = 0; x < cards.length; x++) {
+		if (isNaN(Number(cards[x][0]))) { // Check for  A, K, Q, J or T
+			if (cards[x][0] === 'A') { // If A..
+				value += 11;
+				++numAces;
+			} else { // If K, Q, J or T
+				value += 10;
+			}
+		} else { // If 1, 2, 3, 4, 5, 6, 7, 8, 9
+			value += Number(cards[x][0]);
+		}
+	}
+
+	// If we have any Aces count down their value from 11 to 1 if total value is over 21
+	while (numAces !== 0 && value > 21) {
+		value -= 10;
+		--numAces;
+	}
+
+	return value;
 }
 
 function dealCardTo(player, numberOfCards) {
@@ -61,41 +92,13 @@ function printHands() {
 
 	// Add the players card to printMessage
 	for (var i = 0; i < playerCards.length; i++) {
-		printMessage += playerCards[i];
+		printMessage += playerCards[i] + ' ';
 	}
 
 	// Skriv ut korten
-	console.log('your cards: ' + printMessage);
+	console.log('your cards: ' + printMessage + '\n Total value: ' +
+		getValue(playerCards));
 	console.log('computers cards: ' + computerCards[0]);
-}
-
-
-function getValue(cards) {
-	var value = 0;
-	var numAces = 0;
-
-	// Check through cards and assign value
-	for (var x = 0; x < cards.length; x++) {
-		if (isNaN(Number(cards[x][0]))) { // Check for  A, K, Q, J or T
-			if (cards[x][0] === 'A') { // If A..
-				value += 11;
-				++numAces;
-			} else { // If K, Q, J or T
-				value += 10;
-			}
-		} else { // If 1, 2, 3, 4, 5, 6, 7, 8, 9
-			value += Number(cards[x][0]);
-		}
-	}
-
-	// If we have any Aces count down their value from 11 to 1 if total value is over 21
-	while (numAces !== 0 && value > 21) {
-		console.log('Counting down aces');
-		value -= 10;
-		--numAces;
-	}
-
-	return value;
 }
 
 
