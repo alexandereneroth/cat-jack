3a69f1d9578b768df49dd92a6a4e7e788fa78936
alexander-eneroth <89alexius@gmail.com>
2014-09-27 21:07:24 +0200

    added a reward message, which is a little awkward atm

--- js/game.dealer.js
+++ js/game.dealer.js
@@ -104,6 +104,28 @@ game.createDealer = function (dealerName) {
 			message = message + 'Tied, with a hand value of ' + winningHandValue + '!\n';
 		} else if (winnerNames.length === 1) {
 			message = winnerNames[0] + ' won with ' + winningHandValue + '!';
+			alert(
+				'\n' +
+				'.   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .\n' +
+				'.\n' +
+				'.                           Yay!,                         .\n' +
+				'.                        you won!                          .\n' +
+				'.                                                                                              .\n' +
+				'.                        .-o=o-.                            .\n' +
+				'.                 ,   /=o=o=o=\\ .-.                           .\n' +
+				'.               _|\\|o=O=o=O=|    \\                        .\n' +
+				'.          __.\'   a`\\o=o=o=(`\\    /                     .\n' +
+				'.          \'.    a 4/`|.---"""\'`\\  \\ ;\'`)  .---.             .\n' +
+				'.             \\    .\'   /      .---\'    |_.\'   /  .-._)           .\n' +
+				'.              `)   _.\'      /        /`-.__.\'  /                 .\n' +
+				'.                `\'-.____;        /\'-._____.-\'                 .\n' +
+				'.                           `"""`                                    .\n' +
+				'.                                                                                      .\n' +
+				'.                                                                                      .\n' +
+				'.                                                                                      .\n' +
+				'.    .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .\n' +
+				'                                       ASCII art by jgs'
+			);
 		} else {
 			message = 'Everyone busted!';
 		}
