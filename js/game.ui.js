29700ce36b544abb692f2c341468177f38029555
alexander-eneroth <89alexius@gmail.com>
2014-10-06 16:16:38 +0200

    made showCards()

--- js/game.ui.js
+++ js/game.ui.js
@@ -1,14 +1,30 @@
 'use strict';
 
-game.ui = function () {
+game.ui = (function () {
 	var that = {
 		makeCardImgTag: function (card) {
 			var cardImg = document.createElement('img');
 			cardImg.src = card.url;
 			cardImg.style.height = '111px';
 			return cardImg;
+		},
+		showCards: function (playerType, cardsToShow) {
+			if (playerType === 'player' || playerType === 'computer') {
+
+				var cardElements = [];
+				var cardImgTag;
+				for (var i = 0; i < cardsToShow.length; i++) {
+					cardImgTag = that.makeCardImgTag(cardsToShow[i]);
+					cardElements.push(cardImgTag);
+				}
+				$('#player').append(cardElements);
+
+
+			} else {
+				throw 'playerType has to be either "computer" or "player"';
+			}
 		}
 	};
 
 	return that;
-};
+}());
