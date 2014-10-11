87a25c34aaac0c04fa60cb44d3a9f4244081aeac
alexander-eneroth <89alexius@gmail.com>
2014-10-11 13:39:36 +0200

    put the public functions in an object

--- js/public_functions.js
+++ js/public_functions.js
@@ -1,6 +1,9 @@
 'use strict';
 
-// TODO: Is this really the best place for this function?
-function isNumber(value) {
-	return !isNaN(Number(value));
-}
+// TODO: Is this really the best place for this function? - i believe it's pretty decent atleast.
+var cjTools = { //cj for CatJack
+
+	isNumber: function (value) {
+		return !isNaN(Number(value));
+	}
+};
