933c045a9abccefe1b8887f65ff86388899b50cb
Jitan Elis <jitan.elis@gmail.com>
2014-10-12 23:43:22 +0200

    added cjTools in jshint ignore

--- js/public_functions.js
+++ js/public_functions.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// TODO: Is this really the best place for this function? - i believe it's pretty decent atleast.
+// TODO: Is this really the best place for this function? 
+// - i believe it's pretty decent atleast.
 var cjTools = { //cj for CatJack
 
 	isNumber: function (value) {
