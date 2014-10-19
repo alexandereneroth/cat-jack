441b44cc739217022a428140c2a804b350e46eed
Jitan Elis <jitan.elis@gmail.com>
2014-10-19 21:18:15 +0200

    Finished refactoring on deck.js

--- js/game.deck.js
+++ js/game.deck.js
@@ -1,33 +1,40 @@
 'use strict';
+
+// Constructor function to get a deck of cards for the game.
+// Returns shuffled array with all the cards.
 game.getDeck = function (numberOfDecks) {
-	var that = {};
-	that.cards = [];
+	/* * * * * *
+	 * Private *
+	 * * * * * */
+	var cards = [];
+	createDeck();
+	shuffle();
 
-	// This function exists so we can type deck.pop(), 
-	// instead of deck.cards.pop()
-	that.pop = function () {
-		return that.cards.pop();
-	};
-
-	function addCards() {
+	function createDeck() {
+		var newDeck = [];
 		var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 		var suits = ['H', 'S', 'C', 'D'];
 
-		for (var i = 0; i < numberOfDecks; ++i) {
-			for (var j = 0; j < ranks.length; j++) {
-				for (var k = 0; k < suits.length; k++) {
-					that.cards.push(game.getCard(ranks[j], suits[k]));
-				}
-			}
+		// Push all the cards into the a new deck
+		ranks.forEach(function (rank) {
+			suits.forEach(function (suit) {
+				newDeck.push(game.getCard(rank, suit));
+			});
+		});
+
+		// Duplicate the the deck to the main cards array based on numerOfDecks
+		while (numberOfDecks > 0) {
+			cards = cards.concat(newDeck);
+			numberOfDecks -= 1;
 		}
 	}
 
-	function shuffle(cards) {
+	function shuffle() {
 		var currentIndex = cards.length,
 			temporaryValue, randomIndex;
 
 		// While there remain elements to shuffle...
-		while (0 !== currentIndex) {
+		while (currentIndex > 0) {
 			// Pick a remaining element...
 			randomIndex = Math.floor(Math.random() * currentIndex);
 			currentIndex -= 1;
@@ -39,7 +46,16 @@ game.getDeck = function (numberOfDecks) {
 		}
 		return cards;
 	}
-	addCards();
-	that.cards = shuffle(that.cards);
-	return that;
+
+	/* * * * * *
+	 * Public  *
+	 * * * * * */
+	var deck = {}; // Public functions and properties are returned with this object
+
+	// This function exists so we can type deck.pop() 
+	// instead of deck.cards.pop()
+	deck.pop = function () {
+		return cards.pop();
+	};
+	return deck;
 };
